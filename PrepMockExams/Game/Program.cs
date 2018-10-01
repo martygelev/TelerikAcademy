2970a85c82534a9cfd98dde0502e8cf4aa5f942d
martygelev <martygelev@gmail.com>
2018-10-01 13:01:09 +0300

    c# tasks

---
+++ PrepMockExams/Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int third = input % 10;
+            int second = (input / 10) % 10;
+            int first = input / 100;
+
+            int sum = first + second + third;
+            int ch1 = first * second + third;
+            int ch2 = first + second * third;
+            int mult = first * second * third;
+
+            int max = Math.Max(sum, Math.Max(ch1, Math.Max(ch2, mult)));
+
+            Console.WriteLine(max);
+        }
+    }
+}
