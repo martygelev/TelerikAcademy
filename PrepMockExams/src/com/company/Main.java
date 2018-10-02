f6e4b2adb98cbd5074705507e47e1ed36ff81f53
martygelev <martygelev@gmail.com>
2018-10-02 14:41:56 +0300

    java files  Please enter the commit message for your changes. Lines starting

--- PrepMockExams/src/com/company/Main.java
+++ PrepMockExams/src/com/company/Main.java
@@ -1,10 +1,22 @@
-package com.company;
-
 import java.util.Scanner;
 
-public class Main {
+public class CalculateFactorial {
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+
+        int inputNumber = in.nextInt();
+        double x = in.nextDouble();
+
+        double fact = 1;
+        double total = 1;
+
+        for (int i = 1; i <= inputNumber; i++) {
+
+            fact *= i;
+            total += fact / Math.pow(x, i);
+        }
+        System.out.printf("%.5f", total);
     }
+
 }
