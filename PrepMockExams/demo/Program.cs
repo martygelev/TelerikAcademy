18c33a61bea14d1d16cf0a69984269f7262a7348
martygelev <martygelev@gmail.com>
2018-10-04 13:15:58 +0300

    more

---
+++ PrepMockExams/demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+			Console.WriteLine(155);
+        }
+    }
+}
